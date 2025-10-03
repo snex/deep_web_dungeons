8528ccf417b3133dfb138dc77f524d14eb01c0af
David Havlicek <snex00@protonmail.com>
2025-10-03 12:52:09 -0700

    update test mixins

--- tests/mixins.py
+++ tests/mixins.py
@@ -14,6 +14,7 @@ from typeclasses.objects import (
     Shield,
     WeaponObject,
 )
+from world.enums import WieldLocation
 
 
 class AinneveTestMixin:
@@ -30,22 +31,34 @@ class AinneveTestMixin:
         self.helmet = create.create_object(
             Helmet,
             key="helmet",
+            attributes=[("inventory_use_slot", WieldLocation.HEAD)]
         )
         self.shield = create.create_object(
             Shield,
             key="shield",
+            attributes=[("inventory_use_slot", WieldLocation.SHIELD_HAND)]
         )
         self.armor = create.create_object(
             ArmorObject,
             key="armor",
+            attributes=[("inventory_use_slot", WieldLocation.BODY)]
         )
         self.weapon = create.create_object(
             WeaponObject,
             key="weapon",
+            attributes=[("inventory_use_slot", WieldLocation.WEAPON_HAND)]
         )
         self.big_weapon = create.create_object(
             WeaponObject,
             key="big_weapon",
-            attributes=[("inventory_use_slot", enums.WieldLocation.TWO_HANDS)],
+            attributes=[("inventory_use_slot", WieldLocation.TWO_HANDS)],
         )
-        self.item = create.create_object(Object, key="backpack item")
+        self.item = create.create_object(
+            Object,
+            key="backpack item",
+        )
+        self.item2 = create.create_object(
+            Object,
+            key="backpack item 2",
+            attributes=[("size", 5)]
+        )
